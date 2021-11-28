e946c0573b0cd337f6dd8a72343115e86fe646c4
Ca Du <94875643+Makckako@users.noreply.github.com>
2021-11-28 00:53:41 +0000

    Adicionado exercicio prompt

---
+++ aula_01/exercicios/exercicioOp.js
@@ -0,0 +1,13 @@
+console.log('Hello World!')
+
+const prompt = require('prompt')
+prompt.start()
+
+const mult = () => {
+prompt.get(['a', 'b', 'c'], (err,result) => {
+console.log(parseInt(result.a) * parseInt(result.b) * parseInt(result.c
+))
+})
+}
+
+mult() 
